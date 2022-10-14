b8a6b403bba29ad93e678c19b604a3d306db1963
Ololo <vsvktor@gmail.com>
2022-10-14 09:23:34 +0300

    add TabLayout

---
+++ app/src/main/java/com/hviksor/pizzaapp/presentor/screen/StartFragment.kt
@@ -0,0 +1,19 @@
+package com.hviksor.pizzaapp.presentor.screen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hviksor.pizzaapp.R
+
+class StartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+}
